d5e9c0203c3a45759275da4f8a19091fc75e1960
Sashank <sgondala2@gmail.com>
2015-10-05 19:39:28 +0530

    Added awesome method for single LCA

--- LCA.cpp
+++ LCA.cpp
@@ -24,45 +24,86 @@ struct node{
 
 int val1, val2;
 
-node* LCA(bool &firstBool, bool &secondBool, node* root){ 
+
+// My method - Sollu 
+
+// node* LCA(bool &firstBool, bool &secondBool, node* root){ 
+// 	if(root==NULL){
+// 		return NULL;
+// 	}
+
+// 	bool firstBoolCopy = firstBool, secondBoolCopy = secondBool;
+// 	node* temp = LCA(firstBoolCopy, secondBoolCopy, root->left);
+// 	if(temp!=NULL){
+// 		return temp;
+// 	}
+
+// 	bool firstBoolCopy2 = firstBool, secondBoolCopy2 = secondBool;
+// 	temp = LCA(firstBoolCopy2,secondBoolCopy2,root->right);
+// 	if(temp!=NULL){
+// 		return temp;
+// 	}
+
+// 	firstBool = firstBoolCopy || firstBoolCopy2;
+// 	secondBool = secondBoolCopy || secondBoolCopy2;
+// 	if(root->val == val1){
+// 		firstBool = true;
+// 	}
+// 	if(root->val == val2){
+// 		secondBool = true;
+// 	}
+// 	if(firstBool && secondBool){
+// 		return root;
+// 	}
+// 	else{
+// 		return NULL;
+// 	}
+// }
+
+
+// Awesome method
+
+node* lcaHelper(int val1, int val2, bool &b1, bool &b2, node* root){
 	if(root==NULL){
 		return NULL;
 	}
-
-	bool firstBoolCopy = firstBool, secondBoolCopy = secondBool;
-	node* temp = LCA(firstBoolCopy, secondBoolCopy, root->left);
-	if(temp!=NULL){
-		return temp;
-	}
-
-	bool firstBoolCopy2 = firstBool, secondBoolCopy2 = secondBool;
-	temp = LCA(firstBoolCopy2,secondBoolCopy2,root->right);
-	if(temp!=NULL){
-		return temp;
-	}
-
-	firstBool = firstBoolCopy || firstBoolCopy2;
-	secondBool = secondBoolCopy || secondBoolCopy2;
-	if(root->val == val1){
-		firstBool = true;
-	}
-	if(root->val == val2){
-		secondBool = true;
-	}
-	if(firstBool && secondBool){
+	if(root->val==val1){
+		b1=true;
 		return root;
 	}
+	if(root->val==val2){
+		b2=true;
+		return root;
+	}
+	node* lcaLeft = lcaHelper(val1,val2,b1,b2,root->left);
+	node* lcaRight = lcaHelper(val1,val2,b1,b2,root->right);
+	if(lcaLeft!=NULL && lcaRight!=NULL){
+		return root;
+	}
+	if(lcaLeft!=NULL){
+		return lcaLeft;
+	}
 	else{
-		return NULL;
+		return lcaRight;
 	}
 }
 
-void inOrder(node* root){
-	if(root==NULL){return;}
-	inOrder(root->left);
-	cout<< root->val <<" ";
-	inOrder(root->right);
-	return;
+bool find(node* root, int val){
+	if(root==NULL){
+		return false;
+	}
+	if(root->val == val){
+		return true;
+	}
+	return (find(root->left,val) || find(root->right,val));
+}
+
+node* lca(int val1, int val2, bool &b1, bool &b2, node* root){
+	node* temp = lcaHelper(val1, val2, b1, b2, root);
+	if(b1&&b2 || b1&&find(temp,val2) || b2&&find(temp,val1)){
+		return temp;
+	}
+	return NULL;
 }
 
 int main(){
@@ -73,12 +114,10 @@ int main(){
 	root->left->right = new node(5);
 	root->right->left = new node(6);
 	root->right->right = new node(7);
-	inOrder(root);
-	cout<<endl;
 	while(true){
 		cin>> val1 >> val2;
 		bool isPresent1 = false, isPresent2 = false;
-		node* temp = LCA(isPresent1, isPresent2, root);
+		node* temp = lca(val1, val2, isPresent1, isPresent2, root);
 		if(temp!=NULL){
 			cout<< temp->val <<endl;
 		}
