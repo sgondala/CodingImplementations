95d3e016af22e1aa27feeb6d973a0d9c287922ae
Sashank <sgondala2@gmail.com>
2015-10-15 01:46:36 +0530

    Random

--- codeBook.cpp
+++ codeBook.cpp
@@ -9,6 +9,13 @@ using namespace std;
 
 typedef long long ll;
 
+#define trace1(x)							cerr << #x << ": " << x << endl;
+#define trace2(x, y)						cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)						cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       			cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    			cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) 			cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
 //GCD of 2 int's
 int gcd(int a, int b){
 	if(b==0){return a;}
---
+++ helper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void val(void* a){
+
+}
+
+int main(){
+
+}
