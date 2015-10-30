82f2c6d3d09fb8a0e41f629795d35fd9596e21d7
Sashank <sgondala2@gmail.com>
2015-10-30 15:01:05 +0530

    Heap Done

--- heap.cpp
+++ heap.cpp
@@ -49,13 +49,37 @@ void heapSort(int a[], int heapSize){ //We use maxHeaps for heapSort... Not min 
 	}
 }
 
+void increaseKeyValue(int a[], int index, int newVal){
+	if(a[index]>=newVal){
+		cout<<"Give correct key value" <<endl;
+		return;
+	}
+	a[index] = newVal;
+	while(index>1 && a[parent(index)]<a[index]){
+		swap(a[index], a[parent(index)]);
+		index = parent(index);
+	}
+}
+
+int extractMax(int a[], int &heapSize){
+	int returnVal = a[1];
+	swap(a[1],a[heapSize]);
+	heapSize--;
+	makeHeap(a, 1, heapSize);
+	return returnVal;
+} 
+
+void addVal(int a[], int val, int &heapSize){
+	heapSize++;
+	a[heapSize] = -100000000;
+	increaseKeyValue(a, heapSize, val);
+}
+
 int main(){
 	int a[] = {10,1,3,4,2,5,6,7,8,9};
 	int heapSize = 5; //Heap starts at 1 always - So 1,2,3,4,5
 	buildHeap(a,heapSize);
 	heapSort(a,heapSize);
-	for(int i=1; i<=heapSize; i++){
-		cout<<a[i]<<" ";
-	}
-	cout<<endl;
+	extractMax(a,heapSize);
+	addVal(a,6,heapSize);
 }
